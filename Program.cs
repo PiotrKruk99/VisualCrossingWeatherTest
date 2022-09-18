42df6d2d437de360bf3bad958a41e44778539d8e
PiotrKruk99-d <biuro@liberezo.pl>
2022-09-18 15:04:49 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+var apiClient = new HttpClient();
+
+Console.Write("Type access key: ");
+var accessKey = Console.ReadLine();
+
+var request = new HttpRequestMessage();
+request.Method = HttpMethod.Get;
+var uriString = $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/nowa%20wie%C5%9B%20brze%C5%BAnio?unitGroup=metric&include=current&key=";
+uriString += accessKey;
+uriString += $"&contentType=json";
+request.RequestUri = new Uri(uriString);
+var response = await apiClient.SendAsync(request);
+
+if (response.IsSuccessStatusCode)
+{
+    var body = await response.Content.ReadAsStringAsync();
+    //Console.WriteLine(body);
+    dynamic weather = JsonConvert.DeserializeObject(body);
+
+    string output = "Actual tepmerature in ";
+    output += weather.resolvedAddress;
+    output += " is ";
+    output += weather.currentConditions.temp;
+    output += " and humidity is ";
+    output += weather.currentConditions.humidity;
+
+    Console.WriteLine(output);
+}
