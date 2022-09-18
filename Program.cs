fa5122ff711abbb4b714a654b9a03faa09e23137
PiotrKruk99-d <biuro@liberezo.pl>
2022-09-19 00:35:09 +0200

    added iteration to check multiple cities with typing access key only one time

--- Program.cs
+++ Program.cs
@@ -1,34 +1,56 @@
 ﻿using Newtonsoft.Json;
 
 var apiClient = new HttpClient();
+bool goAnother = true;
 
 Console.Write("Type access key: ");
 var accessKey = Console.ReadLine();
 
-var request = new HttpRequestMessage();
-request.Method = HttpMethod.Get;
-var uriString = $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/nowa%20wie%C5%9B%20brze%C5%BAnio?unitGroup=metric&include=current&key=";
-uriString += accessKey;
-uriString += $"&contentType=json";
-request.RequestUri = new Uri(uriString);
-var response = await apiClient.SendAsync(request);
-
-if (response.IsSuccessStatusCode)
+while (goAnother)
 {
-    var body = await response.Content.ReadAsStringAsync();
-    //Console.WriteLine(body);
-    dynamic weather = JsonConvert.DeserializeObject(body);
+    var request = new HttpRequestMessage();
+    request.Method = HttpMethod.Get;
 
-    string output = "Actual tepmerature in ";
-    output += weather.resolvedAddress;
-    output += " is ";
-    output += weather.currentConditions.temp;
-    output += " and humidity is ";
-    output += weather.currentConditions.humidity;
+    Console.Write("Type city: ");
+    var cityName = Console.ReadLine();
 
-    Console.WriteLine(output);
-}
-else
-{
-    Console.WriteLine("Error while sending request");
+    var uriString = $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+    uriString += cityName;
+    uriString += $"?unitGroup=metric&include=current&key=";
+    uriString += accessKey;
+    uriString += $"&contentType=json";
+    request.RequestUri = new Uri(uriString);
+    var response = await apiClient.SendAsync(request);
+
+    if (response.IsSuccessStatusCode)
+    {
+        var body = await response.Content.ReadAsStringAsync();
+        //Console.WriteLine(body);
+        dynamic weather = JsonConvert.DeserializeObject(body);
+
+        string output = "Actual tepmerature in ";
+        output += weather.resolvedAddress;
+        output += " is ";
+        output += weather.currentConditions.temp;
+        output += " and humidity is ";
+        output += weather.currentConditions.humidity;
+
+        Console.WriteLine(output);
+    }
+    else
+    {
+        Console.WriteLine("Error while sending request");
+    }
+
+    Console.WriteLine("Check another city? (y/n)");
+    char choice;
+
+    do
+    {
+        choice = Console.ReadKey(true).KeyChar;
+    }
+    while (choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n');
+
+    if (choice != 'Y' && choice != 'y')
+        goAnother = false;
 }
